5cfeb3cea570156f17f3dd4a9baa16d9eca6a2d7
Huy Pham <ngochuy.mmt@gmail.com>
2020-10-18 17:07:35 +0700

    add code jsp-servlet

---
+++ DemoServlet/src/com/vn/controller/Message.java
@@ -0,0 +1,20 @@
+package com.vn.controller;
+
+public class Message {
+	private String msg;
+	
+	public Message(String msg) {
+		super();
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+}
